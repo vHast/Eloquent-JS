a89fdc7bedeec07d17d6f30bf7af23da7ef77e7d
vHast <fedvhast@gmail.com>
2023-04-27 22:15:51 +0300

    Delete operator

--- ChapterFour/02Objects.js
+++ ChapterFour/02Objects.js
@@ -6,3 +6,26 @@ let day1 = {
   squirrel: false,
   events: ["work", "touched tree", "pizza", "running"]
 };
+
+// When an object is writtern over multiple lines, indenting it helps readibility
+
+let descriptions = {
+  work: "Went to work",
+  "touched tree": "Touched a tree"
+};
+
+console.log(descriptions["touched tree"]);
+
+// This means braces have two meaning in JavaScript, at the start of a statement they start a block of statements, in the other position, they describe an object
+
+// Reading a property that doesn't exist will give you the value undefined
+
+// * The delete operator
+
+let anObject = {
+  left: 1,
+  right: 2
+};
+
+delete anObject.left;
+console.log(anObject.left) // undefined
